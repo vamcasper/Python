c1ebe7c7530921c1cfb762bf0e59e03102e6deb4
vamcasper <idrisovmarat@gmail.com>
2021-01-15 12:08:30 +0300

    Домашнее задание 6

---
+++ task-6_1.py
@@ -0,0 +1,23 @@
+# Задание 1
+
+from time import sleep
+
+
+class TrafficLight:
+    __color = ['Красный', 'Желтый', 'Зеленый']
+
+    def running(self):
+        i = 0
+        while i < 3:
+            print(f'{self.__color[i]} сигнал светофора!')
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(5)
+            elif i == 2:
+                sleep(4)
+            i += 1
+
+
+TrafficLight = TrafficLight()
+TrafficLight.running()
